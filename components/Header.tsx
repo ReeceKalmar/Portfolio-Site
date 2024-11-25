f7ddec16be786eb6a3ed39fcf50228a9bd42076f
Reece Kalmar <ralmarkalmar@gmail.com>
2024-11-25 11:09:42 -0700

    Finished the home page

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,28 +6,30 @@ import Image from 'next/image';
 import localFont from 'next/font/local';
 
 export const caskaydiaCove = localFont({
-    src:[
-    {
-      path: './fonts/CaskaydiaCoveNerdFont-Regular.ttf',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-caskaydia-cove',
-  });
-  
+    src: [
+        {
+            path: './fonts/CaskaydiaCoveNerdFont-Regular.ttf',
+            weight: '700',
+            style: 'normal',
+        },
+    ],
+    variable: '--font-caskaydia-cove',
+});
+
 
 const Header: React.FC = () => {
     return (
-        <header className={styles.navContainer}>
-            <nav className={styles.lhTextContainer}>
-                <div className={styles.nameText}>ReeceKalmar();</div>
-            </nav>
-            <Image className={styles.centerLogo} src="/arch.png" alt="Logo" width={96} height={96} />
-            <nav className={styles.rhTextContainer}>
-                <div className={styles.resumeText}>Resume</div>
-                <div className={styles.projectText}>Projects</div>
-            </nav>
+        <header className={styles.headerContainer}>
+            <div className={styles.navContainer}>
+                <nav className={styles.lhTextContainer}>
+                    <div className={styles.nameText}>ReeceKalmar();</div>
+                </nav>
+                <Image className={styles.centerLogo} src="/arch.png" alt="Logo" width={96} height={96} />
+                <nav className={styles.rhTextContainer}>
+                    <div className={styles.resumeText}>Resume</div>
+                    <div className={styles.projectText}>Projects</div>
+                </nav>
+            </div>
         </header>
     );
 };
